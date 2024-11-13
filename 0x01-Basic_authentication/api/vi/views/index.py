b7bde02297d3a66807f93d45fa8e3818e2d6276a
UzoCode <akosabenedict@gmail.com>
2024-11-13 06:12:38 +0100

    Basic Authentication

---
+++ 0x01-Basic_authentication/api/vi/views/index.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" The Module of Index views
+"""
+from flask import jsonify, abort
+from api.v1.views import app_views
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ The GET /api/v1/status
+    Returns:
+      - the status of the API
+    """
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> str:
+    """ The GET /api/v1/stats
+    Returns:
+      - the number of each objects
+    """
+    from models.user import User
+    stats = {}
+    stats['users'] = User.count()
+    return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ The GET /api/v1/unauthorized
+    Returns:
+      - Aborts
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ The GET /api/v1/forbidden
+    Returns:
+      - Aborts
+    """
+    abort(403)
